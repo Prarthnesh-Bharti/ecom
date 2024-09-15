2f105bba3f9a5f7c051bdce85cc657d9b86995cf
Prarthnesh Bharti <128624879+Prarthnesh-Bharti@users.noreply.github.com>
2024-09-15 07:03:30 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,24 +1,28 @@
 const mongodb = require('mongodb');
-
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connectToDatabase() {
-  let dburl = 'mongodb://0.0.0.0:27017';
-  if(process.env.MONGODB_URL){
-    dburl = process.env.MONGODB_URL ;
-  }
+  try {
+    let dburl = 'mongodb://localhost:27017'; // Use localhost instead of 0.0.0.0
+    if (process.env.MONGODB_URL) {
+      dburl = process.env.MONGODB_URL;
+    }
     
-  const client = await MongoClient.connect(dburl);
-  database = client.db('online-shop');
+    const client = await MongoClient.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true });
+    database = client.db('online-shop');
+    console.log('Connected to the database successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    throw new Error('Could not connect to database!');
+  }
 }
 
 function getDb() {
   if (!database) {
-    throw new Error('You must connect first!');
+    throw new Error('You must connect to the database first!');
   }
-
   return database;
 }
 
