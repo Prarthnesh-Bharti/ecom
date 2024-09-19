febba841be53e6b5a05fc17714894901dd91bb39
Satyam Raj <satyamraj5253@gmail.com>
2024-09-19 10:29:07 +0530

    gitignore

--- data/database.js
+++ data/database.js
@@ -4,14 +4,14 @@ let database;
 
 async function connectToDatabase() {
   const dbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017';
-  const dbName = 'online-shop';  // Specify the database name here
+  const dbName = 'online-shop';  // database name 
 
   try {
     const client = await MongoClient.connect(dbUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    database = client.db(dbName); // Select the database
+    database = client.db(dbName); 
     console.log('Connected to the database successfully');
   } catch (error) {
     console.error('Database connection failed:', error);
